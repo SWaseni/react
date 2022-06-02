1d658db9f8f70337c7a5f7f1d98f90097d1a2ffb
Sami <samiwafula3257@gmail.com>
2022-06-03 01:43:45 +0300

    edit req contains arg for request data, res ar handles result add handler for login path call res.send with JS object containing token

--- login/server.js
+++ login/server.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors')
 const app = express();
 
 app.use(cors());
+
+app.use('/login', (req, res) => {
+  res.send({
+    token: 'test123'
+  });
+});
