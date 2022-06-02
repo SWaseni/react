238ceb98983a5c5706b80eb390ba3474dc494b2a
Sami <samiwafula3257@gmail.com>
2022-06-03 01:01:01 +0300

    edit Add project boilerplate Modify project structure

--- login/src/App.js
+++ login/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Dashboard from '../Dashboard/Dashboard';
-import Preferences from '../Preferences/Preferences';
+import Dashboard from './components/Dashboard/Dashboard';
+import Preferences from './components/Preferences/Preferences';
 
 function App() {
   return (
