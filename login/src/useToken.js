9b7dbb89ec2cbc6977eae8ac9c72658c4197f15a
Sami <samiwafula3257@gmail.com>
2022-06-03 03:31:53 +0300

    edit useToken copy setToken from App.js -convert to arrow function(saveToken) save token to session - setToken

--- login/src/useToken.js
+++ login/src/useToken.js
@@ -6,6 +6,11 @@ export default function useToken() {
     const userToken = JSON.parse(tokenString);
     return userToken?.token
   };
+
   const [token, setToken] = useState(getToken());
 
+  const saveToken = userToken => {
+    sessionStorage.setItem('token', JSON.stringify(userToken));
+    setToken(userToken.token);
+  };
 }
